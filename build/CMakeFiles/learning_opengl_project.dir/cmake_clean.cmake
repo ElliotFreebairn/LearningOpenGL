f8e9e0e291204ff390e1e8b650ee255269a0a948
Elliot <elliotfreebairn1@gmail.com>
2024-08-26 21:38:29 +0100

    Inital commit with where im at learnng OpenGL

---
+++ build/CMakeFiles/learning_opengl_project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning_opengl_project.dir/src/glad.c.o"
+  "CMakeFiles/learning_opengl_project.dir/src/glad.c.o.d"
+  "CMakeFiles/learning_opengl_project.dir/src/main.cpp.o"
+  "CMakeFiles/learning_opengl_project.dir/src/main.cpp.o.d"
+  "learning_opengl_project"
+  "learning_opengl_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/learning_opengl_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/learning_opengl_project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for learning_opengl_project.
+# This may be replaced when dependencies are built.
